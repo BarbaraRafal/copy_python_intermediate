523975dbc451be2359ba1f4709c4dc4324a07e49
Barbara Rafał <barbara.rafal@gmail.com>
2021-01-31 13:20:34 +0100

    cats_list added

--- SDA_exercises_OOP_1/cat.py
+++ SDA_exercises_OOP_1/cat.py
@@ -1,6 +1,8 @@
 class Cat:
-    def __init__(self, cat_name: str):
+    def __init__(self, cat_name: str, sound='Miauu'):
         self.cat_name = cat_name
+        self.sound = sound
+
 
     def make_sound(self) -> str:
-        return f'Name {self.cat_name} makes miau'
+        return f'Cat {self.cat_name} makes {self.sound}'
--- SDA_exercises_OOP_1/main.py
+++ SDA_exercises_OOP_1/main.py
@@ -2,9 +2,27 @@ from SDA_exercises_OOP_1.cat import Cat
 
 
 def main():
-    cat1 = Cat("Grisza")
-    sound: str = cat1.make_sound()
-    print(sound)
+    cat1 = Cat("Grisza", "Maiuuu")
+    cat2 = Cat("Indira", "Mrrrr")
+    cat3 = Cat("Felix", "Grrrrr")
+    cat4 = Cat("Garfield", "Brrrrr")
+
+    # cats = [cat1, cat2, cat3, cat4]
+    # for cat in cats:
+    #     print(cat.make_sound())
+
+    cats_list = []
+    cats_list.append(cat1)
+    cats_list.append(cat2)
+    cats_list.append(cat3)
+    cats_list.append(cat4)
+
+    for cat in cats_list:
+        print(cat.make_sound())
+
+
+
+
 
 
 if __name__ == "__main__":
