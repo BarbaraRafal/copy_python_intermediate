7c40f3054af89df57837608383f2c879082c07dd
Barbara Rafał <barbara.rafal@gmail.com>
2021-02-21 12:57:33 +0100

    ex3

--- lambda_exercises/main.py
+++ lambda_exercises/main.py
@@ -1,9 +1,13 @@
-from python_intermediate_training.context_manager.exercise_1 import exercise_1
+from python_intermediate_training.lambda_exercises.exercises import exercise_1
+from python_intermediate_training.lambda_exercises.exercises import exercises_2_3
+from python_intermediate_training.lambda_exercises.exercise_4 import exercise_4
 
 
 def main():
     exercise_1()
+    exercises_2_3()
+    exercise_4()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
