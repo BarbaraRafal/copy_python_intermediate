e34c79681c3ae4dc3ba3cefe4cd5aa8f0958029c
Barbara Rafał <barbara.rafal@gmail.com>
2021-02-21 11:36:57 +0100

    execises

---
+++ context_manager/exercise_1.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def open_file(path: str, mode='a'):
+    print("opening file in progress...")
+    fd = open(path, mode)
+    yield fd  # zwracmy to do with
+    f.close()  # tu musimy zawołac to co sie dzieje po wyjsciu  with
+    print("file is closing...")
+
+
+def exercise_1():
+    try:
+        with open_file("./file_to_open.txt") as fd:
+            fd.write("dopisz mnie do pliku")
+    except IOError as ioe:
+        print(f" Problem with writing to file {ioe.args}")
+
+
+
+
---
+++ context_manager/main.py
@@ -0,0 +1,10 @@
+from python_intermediate_training.context_manager.exercise_1 import exercise_1
+from python_intermediate_training.context_manager.exercise_2 import exercise_2
+
+
+def main():
+    # exercise_1()
+    exercise_2()
+
+if __name__ == '__main__':
+    main()
