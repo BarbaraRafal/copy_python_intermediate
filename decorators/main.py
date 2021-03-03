f74e57553112b0bc28fe1295a1e2e7ccd716b7eb
Barbara Rafał <barbara.rafal@gmail.com>
2021-03-03 17:31:39 +0100

    solutions

--- decorators/main.py
+++ decorators/main.py
@@ -6,22 +6,23 @@ def main():
     # a, b = print_hello_world(1, 2)
     # print(f'In main: {a} {b}')
 
-    #  exercise 1
-    # def wrap_before_and_after(func):
-    #     def wrapper(*args, **kwargs):
-    #         print('Before')
-    #         result = func(*args, **kwargs)
-    #         print('After')
-    #         return result
-    #
-    #     return wrapper
-    #
-    #
-    # @wrap_before_and_after
-    # def print_hello_world(a, b):
-    #     print("Hello world!")
-    #     print(f'{a, b}')
-    #     return a, b
+     # exercise 1
+    def wrap_before_and_after(func):
+        def wrapper(*args, **kwargs):
+            print('Before')
+            result = func(*args, **kwargs)
+            print('After')
+            return result
+
+        return wrapper
+
+
+    @wrap_before_and_after
+    def print_hello_world(a, b):
+        print("Hello world!")
+        print(f'{a, b}')
+        return a, b
+
 # exerciese 2 wywołanie
     read_file(file_path='./abc.txt')
 
---
+++ decorators/solutions.py
@@ -0,0 +1,99 @@
+from functools import wraps
+
+
+def my_wrapper(func):
+    def wrapper(*args, **kwargs):
+        print('Before')
+        result = func(*args, **kwargs)
+        print('after')
+        return result
+
+    return wrapper
+
+
+def my_simply_wrapper(func):
+    def wrapper():
+        print('Before')
+        result = func()
+        print('after')
+        return result
+
+    return wrapper
+
+
+@my_simply_wrapper
+def hello_function():
+    print('Hello')
+
+
+def check_file_wrapper(func):
+    def inner(*args, **kwargs):
+        path = ''
+        if len(args) > 0:
+            path = args[0]
+        elif kwargs.get('file_path', ''):
+            path = kwargs.get('file_path')
+        import os
+        if path and os.path.exists(path):
+            print('Path exists')
+        elif path:
+            print('Path not exist - file created')
+            from pathlib import Path
+            Path(path).touch()
+        else:
+            print('No argument')
+            import sys
+            sys.exit(1)
+        return func(*args, *kwargs)
+
+    return inner
+
+
+@check_file_wrapper
+def open_file_func(file_path: str):
+    with open(file_path, 'r') as fd:
+        fd.read()
+
+
+def catch_io_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except IOError as e:
+            print(f'io error caught info: {e.args}')
+            return None
+
+    return inner
+
+
+@catch_io_error
+def throw_exception_file():
+    raise IOError('test error')
+
+
+@catch_io_error
+def no_exist_file():
+    path = 'NOT_exist_path'
+    with open(path, 'r') as fd:
+        fd.read()
+
+
+def io_error_wrapper_by_library(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except IOError as e:
+            print(f'io error caught by functools.wraps info: {e.args}')
+
+    return wrapper
+
+
+@io_error_wrapper_by_library
+def ex_5_no_exist_file():
+    path = 'NOT_exist_path'
+    with open(path, 'r') as fd:
+        fd.read()
+
+if __name__ == '__main__':
+    no_exist_file()
